f0e6662f57ca1741b32381e3bdbad2e9ddc360b1
zoe <ZoeOosting@outlook.com>
2023-03-14 10:19:47 +0100

    add parsing files

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+custom mojis are supported using either json or csv
+if no `.json` extension can be found then kaokao will always try to parse them as csv
 use your own files `--files something.json something_else.json`
 
 `something.json` would look like this
@@ -6,4 +8,11 @@ use your own files `--files something.json something_else.json`
 {"value" : " ʅ(ツ)ʃ", "description" : "shrug"},
 {"value" : "（＾－＾）", "description" : "smiling"}
 ]
+```
+
+and `something_else.csv` would be like this
+```
+value,description
+ಠ_ಠ,sceptical
+(╯°□°）╯︵ ┻━┻,tableflip
 ```
--- src/moji.rs
+++ src/moji.rs
@@ -1,4 +1,5 @@
 use crate::config::Config;
+use csv;
 use emojis;
 use serde::{Deserialize, Serialize};
 use serde_json;
@@ -47,7 +48,18 @@ fn load_moji_from_files(cfg: &Config) -> Result<Vec<Moji>, Box<dyn Error>> {
     let mut vec_moji = vec![];
     for p in &cfg.files {
         let filecontents = fs::read_to_string(p)?;
-        let mut m: Vec<Moji> = serde_json::from_str(&filecontents)?;
+        let mut m: Vec<Moji> = if p.ends_with(".json") {
+            serde_json::from_str(&filecontents)?
+        // default to csv
+        } else {
+            let mut mojis = vec![];
+            let mut reader = csv::Reader::from_reader(filecontents.as_bytes());
+            for moji in reader.deserialize() {
+                let moji: Moji = moji?;
+                mojis.push(moji);
+            }
+            mojis
+        };
         vec_moji.append(&mut m);
     }
     Ok(vec_moji)
