86c8f2e81fbaadb7a61d0d36c6010650951d11f7
zoe <ZoeOosting@outlook.com>
2023-03-22 12:37:11 +0100

    make kaomoji list constant

--- src/moji.rs
+++ src/moji.rs
@@ -7,6 +7,7 @@ use serde::{Deserialize, Serialize};
 use std::{error::Error, fs};
 
 mod description;
+mod kaomoji;
 
 #[derive(Serialize, Deserialize, Clone, Eq, PartialEq, Hash)]
 pub struct Moji {
@@ -140,75 +141,11 @@ fn load_recent() -> Result<Vec<Moji>, Box<dyn Error>> {
 }
 
 fn builtin_kaomoji() -> Vec<Moji> {
-    vec![
-        ("(ﾉ≧∇≦)ﾉ ﾐ ┸━┸", "tableflip"),
-        ("(* >ω<)=3ACHOOE!", "sneezing"),
-        ("┐(°‿°)┌", "shrugging"),
-        ("ᕙ(⇀‸↼‶)ᕗ", "flexing"),
-        ("(❁ᴗ͈ ˬ ᴗ͈)ᶻᶻᶻ✧", "sleeping"),
-        ("(:˒[￣] ᵍᵒᵒᵈ ⁿᶦᵍʰᵗ", "good night, sleeping"),
-        ("≖‿≖", "skeptical, dead inside"),
-        ("ಠ_ಠ", "disapproval"),
-        (" /ᐠ｡ꞈ｡ᐟ\\", "cat, kitty"),
-        ("─=≡Σ((( つ•̀ω•́)つLET’SGO!", "let's go"),
-        ("( ˘▽˘)っ♨", "tea, coffee, hot drink, food"),
-        ("(๑¯ิ̑﹃ ¯ิ̑๑)", "hungry"),
-        ("(─‿‿─)", "skeptical, uwu"),
-        ("(ू˃̣̣̣̣̣̣︿˂̣̣̣̣̣̣ ू)", "cry"),
-        ("(✼ X̥̥̥ ‸ X̥̥̥)", "crying"),
-        ("╥﹏╥", "crying"),
-        ("（＾▽＾（＾▽＾＊）", "hugging, friends"),
-        ("（⋆＾－＾⋆）", "smiling, blushing"),
-        ("（＾－＾）", "smiling"),
-        ("（＾³＾）～♪", "whistling"),
-        ("(㇏(•̀ ᢍ •́ )ノ)", "bat"),
-        (" ♡(ᐢ ᴥ ᐢし)", "dog, love"),
-        ("૮ ˘ﻌ˘ ა", "dog, sleeping"),
-        ("(๑•﹏•)⋆* ⁑⋆*", "confused"),
-        ("(ﾉ>｡☆)ﾉ", "surprised"),
-        ("(」ﾟﾛﾟ)｣NOOOooooo━", "surprised"),
-        ("ᵒʰ(⑉・̆⌓・̆⑉)ɴᴏ", "nervous"),
-        ("(*≧m≦*)", "angry"),
-        ("(*･ω･)✎ ", "writing"),
-        ("(๑˃ᴗ˂)ﻭ", "fight, cute, fist"),
-        ("(⌒‿⌒)", "smile"),
-        ("(─‿‿─)♡", "uwu, love"),
-        ("(⌒_⌒;)", "sweat, emberassed"),
-        ("(≧◡≦) ♡", "love, blushing"),
-        ("(；⌣̀_⌣́)", "seething"),
-        ("(」＞＜)」", "you hear about video games"),
-        ("ヾ( ￣O￣)ツ", "yawn, sleepy, bored"),
-        ("(＞﹏＜)", "sour"),
-        ("(・`ω´・) ", "rage, angry"),
-        ("(╬ Ò﹏Ó)", "angry, rage"),
-        ("(T⌓T)", "trolled"),
-        ("(×_×)", "dead"),
-        ("(￢_￢)", "sideeyes"),
-        ("(¬◡¬)✧", "more fab than you"),
-        ("(^_<)〜☆", "wink, sparkle"),
-        ("─=≡Σ((( つ＞＜)つ", "running"),
-        ("ヽ（　＾＾）人（＾＾　）ノ", "high five"),
-        ("(¬_¬'')ԅ(￣ε￣ԅ)", "secret"),
-        ("(ﾉ≧∀≦)ﾉ ‥…━━━★", "wizard beam attack"),
-        ("¯\\_(ツ)_/¯", "shrug"),
-        ("♡₊˚", "hearts and sparkles"),
-        ("(◕‿◕)♡", "love"),
-        ("ᨐᵉᵒʷ", "meow"),
-        ("♡( ◡‿◡ )", "love"),
-        ("(´,,•ω•,,)♡", "love, blushing"),
-        (">〴⋋_⋌〵 ", "rage, cat, angry"),
-        (" (ꈍᴗꈍ)ε｀*)", "kiss, blushing"),
-        ("(ɔˆ ³(ˆ⌣ˆc)", "kiss, smiling"),
-        ("(˵ᵕ̴᷄ ˶̫ ˶ᵕ̴᷅˵)", "flushed, blushing"),
-        ("‧⁺✧(˶´⚰︎`˵)⁺‧", "flushed, blushing, sparkles"),
-        ("(●｀・ω・)=Ｏ)｀-д゜)", "punch"),
-        ("^_^", "smile"),
-        ("T_T", "crying"),
-    ]
-    .iter()
-    .map(|row| Moji {
-        value: row.0.into(),
-        description: row.1.into(),
-    })
-    .collect::<Vec<Moji>>()
+    kaomoji::KAOMOJI
+        .iter()
+        .map(|row| Moji {
+            value: row.0.into(),
+            description: row.1.into(),
+        })
+        .collect::<Vec<Moji>>()
 }
