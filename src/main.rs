93cf20edec9f69bc150d1fb74cd5ffbf94faf45e
zoe-bat <60606719+zoe-bat@users.noreply.github.com>
2023-04-21 14:19:53 +0200

    Revert "dont run rofi again if there is only one variation"

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,11 @@ fn main() {
         Err(reason) => panic!("could not get emoji list because {}", reason),
         Ok(m) => m,
     };
-    let mut index = match rofi::get_index(&cfg, &moji::moji_to_string(&mojilist)) {
+    let index = match rofi::get_index(&cfg, &moji::moji_to_string(&mojilist)) {
         Err(reason) => panic!("rofi failed because: {}", reason),
         Ok(index) => index,
     };
-
-    // the moji might have a variation!
-    if !mojilist[index].variations.is_empty() {
-        mojilist = mojilist[index].variations.clone();
-
-        // open rofi again to pick variation
-        index = match rofi::get_index(&cfg, &moji::moji_to_string(&mojilist)) {
-            Err(reason) => panic!("rofi failed because: {}", reason),
-            Ok(index) => index,
-        };
-    }
-
-    match moji::recent::save(mojilist[index].clone()) {
+    match moji::save_recent(mojilist[index].clone()) {
         Ok(_) => (),
         Err(reason) => panic!("could not save most recently used because: {}", reason),
     }
